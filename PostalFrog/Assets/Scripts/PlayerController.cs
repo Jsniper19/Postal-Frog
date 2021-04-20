ba9768f02330fb3f84de8d001363053b6db2fb33
Jasper Eyers <jaspereyers@hotmail.com>
2021-04-20 12:18:10 +1000

    Upload project files

---
+++ PostalFrog/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float jumpForce = 300.0f;
+    public float speed = 5.0f;
+    public float maxGroundDistance = 1.5f;
+    private bool isGrounded;
+    public bool allowDoubleJump = false;
+    private int amountOfJumps = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void LateUpdate()
+    {
+        isGrounded = Physics.Raycast(transform.position, Vector3.down, maxGroundDistance);
+        if (isGrounded == true)
+        {
+            amountOfJumps = 0;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("space"))
+        {
+			if (isGrounded == false)
+			{
+				transform.position = transform.position + (transform.forward * speed * Time.deltaTime);
+			}
+			
+		}
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position = transform.position + (-transform.forward * speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position = transform.position + (transform.right * speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position = transform.position + (-transform.right * speed * Time.deltaTime);
+        }
+
+        if (Input.GetKeyDown("space"))
+        {
+
+            if (isGrounded)
+            {
+                // turn on the cursor
+                GetComponent<Rigidbody>().AddForce(new Vector3(0.0f, jumpForce, 0.0f));
+                amountOfJumps = 1;
+            }
+            else if (amountOfJumps < 2 && allowDoubleJump)
+            {
+                // turn on the cursor
+                GetComponent<Rigidbody>().AddForce(new Vector3(0.0f, jumpForce, 0.0f));
+                amountOfJumps = 2;
+            }
+
+        }
+    }
+}
