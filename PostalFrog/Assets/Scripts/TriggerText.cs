cc10f2faef01cf7703c0d373293de9c359a27f94
Jasper Eyers <jaspereyers@hotmail.com>
2021-05-11 15:08:03 +1000

    Possibly Done
    added ending and cleaned up

---
+++ PostalFrog/Assets/Scripts/TriggerText.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TriggerText : MonoBehaviour
+{
+    [SerializeField] Text Victory;
+    public AudioClip SoundToPlay;
+    AudioSource soundSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        soundSource = gameObject.GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Victory.text = "Post Delivered!";
+        soundSource.PlayOneShot(SoundToPlay);
+    }
+}
